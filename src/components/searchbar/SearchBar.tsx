6abb51d2cddec26d90153761bde0e9da7bb92519
Erick Sandhu <essandhu22@gmail.com>
2024-09-04 23:20:18 -0500

    fix logo

--- src/components/searchbar/SearchBar.tsx
+++ src/components/searchbar/SearchBar.tsx
@@ -28,8 +28,8 @@ export default function SearchBar() {
       <div className="container mx-auto px-4 flex flex-col md:flex-row items-center justify-between">
         <div className="flex items-center mb-4 md:mb-0">
           <img
-            src="src/images/reddit-alien.svg"
-            alt="Reddit Logo"
+            src="./src/images/reddit-alien.svg"
+            alt="Reddit Alien"
             className="w-12 h-12 mr-4"
           />
           <h1 className="text-3xl font-bold">Reddit Search</h1>
